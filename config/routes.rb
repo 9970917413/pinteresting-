3c83a35b6f2a764432b8eb035546d51cb992a51a
Swapnil Bhosale <swapnil.m.bhosale@gmail.com>
2015-08-13 09:35:10 -0400

    added gems pg and rails_12factor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/contactus'
+
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" #create about path
+  get "contactus" => "pages#contactus"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
